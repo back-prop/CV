0a625340d67cb4b6fad0e5986c4de16c36ccae7a
adyagit <sri.adya@gmail.com>
2019-03-18 22:38:01 -0400

    read write image

--- OpenCVBasics/read_write_image.py
+++ OpenCVBasics/read_write_image.py
@@ -1,7 +1,53 @@
 '''
 # Created by Srikanth Adya at 3/14/2019
 
-Feature: Cofiguration variables set here
-# The HDF5 raw data variables are set in this file
-
 '''
+
+import cv2
+import argparse
+
+#Parse input arguments to get the image file path
+ag = argparse.ArgumentParser()
+ag.add_argument("-i","--image",required=True,help="Path to image file to be read")
+args = vars((ag.parse_args()))
+
+#Read image file using imread function
+image = cv2.imread(args['image'])
+
+#display read in image using imshow function
+cv2.imshow('Image',image)
+cv2.waitKey(0)
+
+# Display the image size
+# Note width is in the second column
+print("Width is %d pixels",image.shape[1])
+print("Height is %d pixels",image.shape[0])
+
+# Write the image to a new file
+cv2.imwrite('NewImage.JPG',image)
+cv2.destroyAllWindows()
+
+# Slice the image into 4 slices
+# To do that we need the center of the image which is given by
+
+cX = int(image.shape[1]/2)
+cY = int(image.shape[0]/2)
+
+print(cY,cX)
+tL = image[0:cY,0:cX]
+tR = image[0:cY,cX:image.shape[1]]
+lL = image[cY:image.shape[0],0:cX]
+lR = image[cY:image.shape[0],cX:image.shape[1]]
+
+cv2.imwrite('TopLeft.png',tL)
+cv2.imwrite('TopRight.png',tR)
+cv2.imwrite('LowerRight.png',lR)
+cv2.imwrite('Lowerleft.png',lL)
+
+# Array Slice to change color of pixel
+
+#image[0:cY,0:cX] = [0,255,0]
+#cv2.imshow('ColoredImage',image)
+#cv2.waitKey(0)
+
+print(image[225,111])
